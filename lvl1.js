7f30b66f0142f91183d688c22de1ccd86569e3ce
Romoute <r.damon@rns.ecolescreatives.com>
2023-05-11 18:02:17 +0200

    premiers tests et suivi du renard

--- lvl1.js
+++ lvl1.js
@@ -1,57 +1,94 @@
-export class lvl1 extends Phaser.Scene{
-    
-    constructor(){
+export class lvl1 extends Phaser.Scene {
+
+    constructor() {
         super("lvl1");
-        this.camera;
     }
 
-    preload(){
+    preload() {
         this.load.spritesheet('nikko', 'assets/characters/nikko.png',
-        {frameWidth : 128, frameHeight : 256});
+            { frameWidth: 128, frameHeight: 256 });
+        this.load.spritesheet('renard', 'assets/characters/renard.png',
+            { frameWidth: 128, frameHeight: 128 });
 
         this.load.image('tileset', 'assets/objects/tileset.png');
-        this.load.tilemapJSON('map1', 'assets/maps/V1Lvl1.json');
-        
+        this.load.tilemapTiledJSON('map1', 'assets/maps/V1Lvl1.json');
+
     }
 
-    create(){
+    create() {
         this.player;
 
         this.map1 = this.add.tilemap('map1');
         this.tileset = this.map1.addTilesetImage('tileset', 'tileset');
-        this.plateformes = this.map1.createLayer('plateformes', this.tileset);
+        this.plateformes = this.map1.createLayer('Calque de Tuiles 1', this.tileset);
 
-        this.plateformes.setCollisionByProperty({estSolid: true});
+        this.plateformes.setCollisionByProperty({ estSolid: true });
 
-        this.player = this.physics.add.sprite(1300, 100, 'nikko')*
+        this.player = this.physics.add.sprite(72, 2288, 'nikko');
         this.player.setCollideWorldBounds(true);
 
+        this.renard = this.physics.add.sprite(700, 2400, 'renard');
+        this.physics.add.existing(this.renard);
+        this.renard.setCollideWorldBounds(true);
+
         this.physics.add.collider(this.player, this.plateformes);
+        this.physics.add.collider(this.renard, this.plateformes);
 
+        this.physics.add.overlap(this.player, this.renard, this.recrute.bind(this));
+        this.interactButton = this.input.keyboard.addKey('E');
 
-        this.camera.startFollow(this.player);
-            this.camera.setDeadzonne(100,100);
-            this.camera.setBounds(0,0,3200,3200);
-    
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.physics.world.setBounds(0, 0, 3840, 3840);
+        this.cameras.main.setBounds(0, 0, 12800, 12800);
+        this.cameras.main.startFollow(this.player);
+
     }
 
-    update(){
-            // ajout des moyens de déplacement du personnage
-        if ((cursors.left.isDown)){ //si la touche gauche est appuyée
-            player.setVelocityX(-160); //alors vitesse négative en X
-        
-        if ((cursors.right.isDown)){ //sinon si la touche droite est appuyée
-                player.setVelocityX(160); //alors vitesse positive en X
-                player.anims.play('right', true); //et animation => droite
+    update() {
+
+
+        if ((this.cursors.left.isDown)) {
+            this.player.setVelocityX(-260);
+            if (this.cursors.up.isDown) {
+                this.player.setVelocityY(-400);
             }
-            
-        if ((cursors.up.isDown)){
-            //si touche haut appuyée ET que le perso touche le sol
-            player.setVelocityY(-200); //alors vitesse verticale négative
-            //(on saute)
-                
+        }
+        else if ((this.cursors.right.isDown)) {
+            this.player.setVelocityX(260);
+            if (this.cursors.up.isDown) {
+                this.player.setVelocityY(-400);
+            }
+        }
+        else if ((this.cursors.up.isDown)) {
+            this.player.setVelocityY(-400);
+        }
+        if (this.player.body.onFloor()) {
+            if (this.cursors.left.isUp && this.cursors.right.isUp) {
+                this.player.setVelocityX(0);
+            }
         }
 
+        if (this.interactButton.isDown) {
+            this.recrute();
+            console.log("recrute")
+        }
+
+        if (this.renard.followingPlayer) {
+            this.renard.body.velocity.x = this.player.body.velocity.x;
+            this.renard.body.velocity.y = this.player.body.velocity.y;
+          }
+
+    }
+
+
+    recrute() {
+        const distance = Phaser.Math.Between(this.player.x,this.renard.x);
+        console.log(distance)
+
+        if (distance < 44000) {
+            this.renard.followingPlayer = true;
         }
     }
+    
 }
--- script.js
+++ script.js
@@ -8,16 +8,16 @@ import { lvl1 as lvl1} from "/lvl1.js"
 
 var config = {
     type: Phaser.AUTO,
-    width: 4160, height: 3456,
+    width: 4160, height: 3840,
     physics: {
         default: 'arcade',
         arcade: {
+            gravity: { y: 600 },
         debug: true
     }},
     pixelArt:true,
     input:{gamepad:true},
-    scene: [lvl1],
-        
-};
-
+    scene: [lvl1],     
+    };
+    
 new Phaser.Game(config);
