007e00a449d5a28b2608df2764eb9130131d4f48
MathisVchia <118368064+MathisVchia@users.noreply.github.com>
2023-06-04 01:21:00 +0200

    Menu démarrage (je voulais tenter une blague avec Menu de resto mais rien trouvé... :( )

--- Menu.js
+++ Menu.js
@@ -5,29 +5,44 @@ export class Menu extends Phaser.Scene {
 
     preload() {
         // Préchargement des ressources nécessaires, comme les images et les sons
+        this.load.image('menu', 'assets/objects/EcranDemarrage.png');
+        this.load.image('play', 'assets/characters/toucheDemarrage.png');
+        this.load.image('playClic', 'assets/characters/toucheDemarrageClic.png');
+        this.load.image('light', 'assets/effects/light.png');
     }
 
     create() {
-        // Création de l'objet fond qui prend l'intégralité de l'écran
-        const fond = this.add.rectangle(0, 0, this.game.config.width, this.game.config.height, 0x000000);
-        fond.setOrigin(0);
+        // Récupérer les dimensions de l'écran
+        const screenWidth = this.cameras.main.width;
+        const screenHeight = this.cameras.main.height;
 
-        // Création de l'objet bouton
-        const bouton = this.add.text(this.game.config.width / 2, this.game.config.height / 2, "Appuyez pour jouer", {
-            fontSize: "32px",
-            color: "#ffffff",
+        // Ajouter l'image du menu avec une taille proportionnelle à l'écran
+        this.add.image(screenWidth / 2, screenHeight / 2, 'menu').setScale(screenWidth / 1920, screenHeight / 1080);
+
+        // Ajouter le bouton play avec une taille proportionnelle à l'écran
+        this.boutonPlay = this.add.image(screenWidth / 2, screenHeight / 1.2, 'play').setScale(screenWidth / 1920, screenHeight / 1080).setInteractive();
+
+        // Gérer l'événement de survol du bouton
+        this.boutonPlay.on('pointerover', () => {
+            this.boutonPlay.setTexture('playClic');
         });
-        bouton.setOrigin(0.5);
-        bouton.setInteractive();
 
-        // Lorsqu'on appuie sur le bouton, on lance le jeu
-        bouton.on("pointerdown", () => {
-            this.scene.start("lvl2");
+        // Gérer l'événement de sortie du survol du bouton
+        this.boutonPlay.on('pointerout', () => {
+            this.boutonPlay.setTexture('play');
+        });
+
+        // Gérer l'événement de clic sur le bouton
+        this.boutonPlay.on('pointerup', () => {
+            this.letsBegin();
         });
     }
 
     update() {
         // Mise à jour du menu principal (si nécessaire)
     }
-}
 
+    letsBegin() {
+        this.scene.start("lvl2");
+    }
+}
